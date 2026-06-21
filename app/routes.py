363a0a0edfaab978f759f415b92a8d44ea3415ee
23f2003837 <23f2003837@ds.study.iitm.ac.in>
2026-06-22 01:44:47 +0530

    add registration and login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,45 @@
-from flask import Blueprint, render_template
+from flask import Blueprint,render_template,request,redirect,url_for,session,flash
+from app.models import User
+from app import db
+from werkzeug.security import generate_password_hash,check_password_hash
 main=Blueprint('main',__name__)
 @main.route("/")
 def home():
     return render_template('home.html')
+@main.route("/login",methods=['GET','POST'])
+def login():
+    if request.method=="POST":
+        email=request.form["email"]
+        password=request.form["password"]
+        existing_user=User.query.filter_by(email=email).first()
+        if existing_user:
+            password_check=check_password_hash(existing_user.password,password)
+            if password_check:
+                flash("Successfully Login")
+                session['user_id']=existing_user.id
+                session['role']=existing_user.role
+                return redirect(url_for("main.home"))
+            else:
+                flash("Incorrect Password")
+                return redirect(url_for("main.login"))
+        flash("Please register")
+        return redirect(url_for("main.register"))
+    return render_template("login.html")
+@main.route("/register",methods=['GET','POST'])
+def register():
+    if request.method=='POST':
+        name=request.form['name']
+        email=request.form['email']
+        password=request.form['password']
+        existing_user=User.query.filter_by(email=email).first()
+        if existing_user:
+            flash("Email already registered, Please Login")
+            return redirect(url_for("main.login"))
+        
+        hashed_password=generate_password_hash(password)
+        new_user=User(name=name,email=email,password=hashed_password,role='trekker')
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Registration successful! Please Login")
+        return redirect(url_for("main.login"))
+    return render_template('register.html')
