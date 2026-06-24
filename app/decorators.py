fa52a82ce260ae156ea2c530c776cc209e906d56
23f2003837 <23f2003837@ds.study.iitm.ac.in>
2026-06-24 16:59:14 +0530

    refactor decorators with check_login and check_blacklisted helpers

--- app/decorators.py
+++ app/decorators.py
@@ -1,13 +1,32 @@
 from functools import wraps
 from flask import session, redirect, url_for, flash
+from app.models import User
+def check_login():
+    if 'user_id' not in session:
+        flash("Please Login!")
+        return redirect(url_for("main.login"))
+    return None
+
+def check_blacklisted(user):
+    if user.is_blacklisted:
+        session.clear()
+        flash("You have been Blacklisted. Please contact Admin")
+        return redirect(url_for('main.home'))
+    return None
 
 def login_as_admin(f):
     @wraps(f)
     def decorated_function(*args,**kwargs):
-        if 'user_id' not in session:
-            flash("Please Login")
-            return redirect(url_for("main.login"))
-        elif session['role']!='admin':
+        #check if the user is logged in  between sessions
+        response=check_login()
+        if response:
+            return response
+        #check if the user got blacklisted between valid sessions
+        user=User.query.get(session['user_id'])
+        blacklisted=check_blacklisted(user)
+        if blacklisted:
+            return blacklisted
+        if session['role']!='admin':
             flash(f'Access Denied')
             return redirect(url_for(f"main.{session['role']}_dashboard"))
         else:
@@ -17,10 +36,16 @@ def login_as_admin(f):
 def login_as_staff(f):
     @wraps(f)
     def decorated_function(*args,**kwargs):
-        if 'user_id' not in session:
-            flash("Please Login")
-            return redirect(url_for("main.login"))
-        elif session['role']!='staff':
+        response=check_login()
+        if response:
+            return response
+        user=User.query.get(session['user_id'])
+        blacklisted=check_blacklisted(user)
+        if blacklisted:
+            return blacklisted
+        
+        
+        if session['role']!='staff':
             flash(f'Access Denied')
             return redirect(url_for(f"main.{session['role']}_dashboard"))
         else:
@@ -30,13 +55,17 @@ def login_as_staff(f):
 def login_as_trekker(f):
     @wraps(f)
     def decorated_function(*args,**kwargs):
-        if 'user_id' not in session:
-            flash("Please Login")
-            return redirect(url_for("main.login"))
-        elif session['role']!='trekker':
+        response=check_login()
+        if response:
+            return response
+        user=User.query.get(session['user_id'])
+        blacklisted=check_blacklisted(user)
+        if blacklisted:
+            return blacklisted
+        
+        if session['role']!='trekker':
             flash(f'Access Denied')
             return redirect(url_for(f"main.{session['role']}_dashboard"))
         else:
             return f(*args,**kwargs)
     return decorated_function
-
